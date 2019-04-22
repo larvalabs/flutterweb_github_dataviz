9153796fd4e436bcf9e8582de4721ed488779d03
John Watkinson <watkinson@gmail.com>
2019-04-22 16:52:36 -0400

    Milestones now plotted.

--- lib/data/milestone.dart
+++ lib/data/milestone.dart
@@ -3,6 +3,7 @@ class Milestone {
   double position;
   String label;
 
+  // todo - time may not be necessary, position is just the position on the graph from 0..1
   Milestone(this.time, this.position, this.label);
 
 }
